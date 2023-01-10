f41b9064f35735d6f18bfe523d8d780dc616c08f
Ikuzot <110021129+Ikuzot@users.noreply.github.com>
2023-01-10 23:30:16 +0700

    Add files via upload

---
+++ Claimbits2/con.php
@@ -0,0 +1,8 @@
+<?php
+$proxy_ip = '45.155.68.129';
+$proxy_port = '8133';
+$Username = 'Tonjo888';
+$Password = 'Tonjo888';
+$cok = "PHPSESSID=1c7s2t2pckmh0u25arji4a7hoi; _gid=GA1.2.1545326312.1673366540; bitmedia_fid=eyJmaWQiOiIwZjZlNTA2ZjQ2NjIzMjAyZDQ1YzI1NDZhNDhlY2VkYyIsImZpZG5vdWEiOiJjODE1ZGNiMDhjZDBmYmViYmUwMDIxMTkxZTkyNGU4OCJ9; cf_clearance=q_4bc8lZE73PGDsQN96IyZtzYomA8yTHmOL_Fl5n4J4-1673366786-0-160; SesHashKey=gddc2v7zs218c7w6; SesToken=ses_id=243540&ses_key=gddc2v7zs218c7w6; AccExist=243540; _ga_J2L7YD89W2=GS1.1.1673366538.1.1.1673366835.0.0.0; _ga=GA1.2.192417502.1673366539";
+$us = "Mozilla/5.0 (Linux; Android 13; Redmi Note 10 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36";
+
